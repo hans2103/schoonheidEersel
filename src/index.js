513f6eaa25468c4b2cbe95cca392c1032a894243
Hans Kuijpers <hans@perfectwebteam.nl>
2019-07-28 10:59:23 +0200

    pending changes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import WebFont from 'webfontloader';
-
+import TagManager from 'react-gtm-module'
 
 WebFont.load({
 	google: {
 		families: ['Nunito:400,700', 'Open+Sans:500', 'sans-serif']
 	}
 });
+
+const tagManagerArgs = {
+	gtmId: 'GTM-M9DK3T'
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
