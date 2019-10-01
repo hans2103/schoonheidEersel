13539068a10149f6c8d0328fa2db0fc35a932bcf
Hans Kuijpers <hans2103@gmail.com>
2019-10-01 15:10:49 +0200

    add link to theeblockrow and add white styling to links

--- src/components/ThreeBlocksRow/ThreeBlocksRow.js
+++ src/components/ThreeBlocksRow/ThreeBlocksRow.js
@@ -5,18 +5,16 @@ import styles from './ThreeBlocksRow.module.scss';
 import { WrapperGrid }  from '../Styled/Styled';
 
 const threeblocksrow = () => (
-
-    <WrapperGrid
-	>
+    <WrapperGrid>
         <div className={styles.Item}>
             <Icon name="envelope-o" />
             <h3>Bel of mail me</h3>
             <ul>
                 <li>
-                    <strong>Telefoon:</strong> 06 - 5310 5406
+					<strong>Telefoon:</strong> <a href="tel:06 - 5310 5406" className="anchorWhite">06 - 5310 5406</a>
                 </li>
                 <li>
-                    <strong>E-mail:</strong> schoonheideersel@gmail.com
+					<strong>E-mail:</strong> <a href="mailto:schoonheideersel@gmail.com" className="anchorWhite">schoonheideersel@gmail.com</a>
                 </li>
             </ul>
         </div>
@@ -31,7 +29,7 @@ const threeblocksrow = () => (
         <div className={styles.Item}>
             <Icon name="map-marker" />
             <h3>Locatie</h3>
-            <p>Krekelbos 14<br/>5521 SV Eersel - Nederland
+			<p><a href="https://www.google.com/maps/dir//Yvonne,+Specialiste+in+Schoonheid,+Krekelbos+14,+5521+SV+Eersel/@51.3515744,5.2686798,13z/data=!4m9!4m8!1m0!1m5!1m1!1s0x47c6ce2c9747c8bd:0xaff07e2002e8c04!2m2!1d5.303699!2d51.351526!3e0" target="_blank" className="anchorWhite">Krekelbos 14<br/>5521 SV Eersel - Nederland</a>
             </p>
         </div>
     </WrapperGrid>
